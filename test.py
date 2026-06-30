a8e5e4637a5e62f49b2551ed5790afdbbda31c7c
unknown <jhahn2747@gmail.com>
2026-06-30 18:21:37 +0900

    kaplan-meier 분석 추가

---
+++ test.py
@@ -0,0 +1,30 @@
+import pandas
+import os
+
+super_dir = os.path.dirname(os.path.abspath(__file__))
+db_dir = os.path.join(super_dir, 'database')
+csv_dir = os.path.join(super_dir, 'completed_csv')
+
+df1 = pandas.read_csv(os.path.join(db_dir, '(LUAD) lung adenocarcinoma.csv'))
+df2 = pandas.read_csv(os.path.join(csv_dir, 'LUAD_50_50_XRCC3.csv'))
+
+print(df1.head())
+print(df2.head())
+
+list1 = []
+list2 = []
+for Patient,Days,Status,Expression,Group in df2[['Patient','Days','Status','Expression','Group']].values:
+    print(Patient, Days, Status, Expression, Group)
+    val1 = df1[df1['sampleID'] == Patient]
+    val2 = df2[df2['Patient'] == Patient]
+
+    if val1['OS.time'].values[0] == val2['Days'].values[0]:
+        print(f"Match found for Patient: {Patient}, {Days}")
+        list1.append((Patient, Days))
+    else:
+        print(f"No match for Patient: {Patient}, {Days}")
+        list2.append((Patient, Days, val1['OS.time'].values[0], val2['Status'].values[0], val1['OS'].values[0]))
+
+print(list1)
+print('--------------------')
+print(list2)
